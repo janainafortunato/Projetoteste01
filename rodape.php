b6b430d19161e94085e8b83f093b8bdfaddb5a65
suellennn <suellenbarbosa.c@hotmail.com>
2018-05-03 13:11:17 -0300

    adicionado biblioteca fontawesome

--- rodape.php
+++ rodape.php
@@ -21,44 +21,34 @@
       <div class="col-md-3 col-sm-6">
         <!--Column1-->
         <div class="footer-pad">
-          <h4>Popular Services</h4>
+          <h4></h4>
           <ul class="list-unstyled">
             <li><a href="#"></a></li>
-            <li><a href="#">Payment Center</a></li>
-            <li><a href="#">Contact Directory</a></li>
-            <li><a href="#">Forms</a></li>
-            <li><a href="#">News and Updates</a></li>
-            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Últimas Notícias</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Ações Sociais</a></li>
           </ul>
         </div>
       </div>
       <div class="col-md-3 col-sm-6">
         <!--Column1-->
         <div class="footer-pad">
-          <h4>Website Information</h4>
+          <h4></h4>
           <ul class="list-unstyled">
-            <li><a href="#">Website Tutorial</a></li>
-            <li><a href="#">Accessibility</a></li>
-            <li><a href="#">Disclaimer</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">FAQs</a></li>
-            <li><a href="#">Webmaster</a></li>
+            <li><a href="#">Curso</a></li>
+            <li><a href="#">Política</a></li>
+            <li><a href="#">Esporte</a></li>
           </ul>
         </div>
       </div>
       <div class="col-md-3 col-sm-6">
         <!--Column1-->
         <div class="footer-pad">
-          <h4>Popular Departments</h4>
+          <h4></h4>
           <ul class="list-unstyled">
-            <li><a href="#">Parks and Recreation</a></li>
-            <li><a href="#">Public Works</a></li>
-            <li><a href="#">Police Department</a></li>
-            <li><a href="#">Fire</a></li>
-            <li><a href="#">Mayor and City Council</a></li>
-            <li>
-              <a href="#"></a>
-            </li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Desenvolvedores</a></li>
+            <i class="fab fa-facebook-square"></i>
           </ul>
         </div>
       </div>
@@ -70,8 +60,9 @@
       <div class="row">
         <div class="col-xs-12">
           <!--Footer Bottom-->
+         
           <p class="text-xs-center">&copy; Copyright 2016 - City of USA.  All rights reserved.</p>
-        </div>
+
       </div>
     </div>
   </div>
