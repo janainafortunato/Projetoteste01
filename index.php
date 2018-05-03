503a77c97db08af8d248415a03130d309bc36879
janiio <janiovinicius17@gmail.com>
2018-05-03 12:31:18 -0300

    incluido rodapé

--- index.php
+++ index.php
@@ -1,63 +1,48 @@
 <?php include "cabeçalho.php";
 ?>
 
-<!-- <div class= "container">
- <br>
- <br>
- <br>
- <br>
-  <center><button type="button"class=#999999>Noticias</button>
-  <button type="button" class=#999999>Cursos</button>
-  <button type="button" class=#999999>Politica</button>
-  <button type="button" class=#999999>Esporte</button>
-  <button type="button" class=#999999>Projetos</button>
-  <button type="button" class=#999999>Equipe</button>
-  <button type="button" class=#999999>Historia</button>
-  <button type="button" class=#999999>Buscar</button>
-  <button type="button" class=#999999><a href="inserir_noticias.php">Inserir noticias</a></button>
-  <button type="button" class=#999999><a href="form.assoc.php">Cadastro</a></button></center>   
-</div> -->
- 
 <main> 
 
   <div class="container">
     <h2>ÚLTIMAS NOTÍCAIS</h2>  
+    
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
+      
       <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
       </ol>
-
-    <!-- Wrapper for slides -->
+    
+      
       <div class="carousel-inner">
         <div class="item active">
           <img src="img/figura01.png" alt="Los Angeles" width="1200" height="1000">
         </div>
-
-      <div class="item">
-        <img src="img/figura02.png" alt="Chicago" width="1200" height="1000">
+    
+        <div class="item">
+          <img src="img/figura02.png" alt="Chicago" width="1200" height="1000">
+        </div>
+    
+        <div class="item">
+          <img src="img/figura02.png" alt="New york" width="1200" height="1000">
+        </div>
       </div>
     
-      <div class="item">
-        <img src="img/figura.03.png" alt="New york" width="1200" height="1000">
-      </div>
+      
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
   </div>
 
   <div class="container">
-    <center><h2>MELHORIAS PARA COMUNIDADE </h2>
+   <center><h2>MELHORIAS PARA COMUNIDADE </h2>
     <p><h4>Capacitação para os membros das associações </h4></p>            
     <img src="img/figura07.png" class="img-circle" alt="Cinque Terre" width="304" height="236"> 
     <p><h4>Cursos de cidadania para moradores do Forte Orage</h4></p>            
@@ -67,94 +52,64 @@
     <p><h4>Reunião geral para os moradores dos bairros do Ssocego e Vila Velha </h4></p>            
     <img src="img/figura10.png" class="img-circle" alt="Cinque Terre" width="304" height="236"> 
   </div></center>
- 
-  <!-- <div id="section1" class="container-fluid">
-  <center><h1>Historia da Instituição</h1></center>
-  <p><h4>Em 2010 foi feito um estudo de mercado para lança uma projeto que divulgasem as instituições que trabalham nas comunidades realizado politicas publicas  que buscam beneficios para os bairros em geral, sendo o municipio de Ilha de Itamaracá escolhido como projeto piloto para a interação das comunidades no site.
-  Deesa forma, a equipe vem trazer uma nova metodologia de comunicação e cidadania com os moradores.</h4></p>
-  <p> </p>
+
+  <div class="container">
+
+    <h2>Deixe seu comentario </h2>
+
+    <div class="media">
+      <div class="media-left">
+        <img src="img/figura04.png" class="media-object" style="width:60px">
+      </div>
+
+      <div class="media-body">
+        <h4 class="media-heading">Maria dos Santos</h4>
+        <p>Gostaria de saber quando vai haver doacões de leite? </p>
+      </div>
+    </div>
+
+    <hr>
+    <div class="media">
+      <div class="media-left">
+        <img src="img/figura05.png" class="media-object" style="width:60px">
+      </div>
+
+      <div class="media-body">
+        <h4 class="media-heading">Fernanda Santos</h4>
+        <p>Gostei bastante do curso de manicure oferecido pela associação do Pilar</p>
+      </div>
+    </div>
+    <hr>
+
+    <div class="media">
+      <div class="media-body">
+        <h4 class="media-heading">José Antonio</h4>
+        <p> como faço para me cadidata para oferecer serviços comunitarios? </p>
+      </div>
+      
+      <div class="media-right">
+        <img src="img/figura06.png" class="media-object" style="width:60px">
+      </div>
+    </div>
+
   </div>
-  <div id="section2" class="container-fluid">
-  <center><h1>Desenvolvedores</h1>
-  <p><h4>Janaina Fortunato da Silva / Email: Janaina.projetos@live.com / Fone: (81) 97322-3016</p>
-  <p>Janio Vinicios Ramos Cajueiro / Email:janiovenicios17@gmail.com / Fone: (81) 98676-9119</p>
-   <p>Suellen Barbosa da Cruz / Email: suellenbarbosa.c@hotmail.com / Fone: (81) 98971-5026</p></h4></center>
-  </div>
-  <div id="section3" class="container-fluid">
-  <center><h1>Missão</h1>
-  <p><h4>Trazer informações para a comunidade com qualidade, simplicidade e com facilidade.</h4> </p></center>
- 
-  </div>
-  <div id="section41" class="container-fluid">
-  <center><h1>Vissão</h1>
-  <p><h4>Ser o melhor site de informação em serviços de cidadania do estado de Pernambu em 3 anos</h4></p></center>
- 
-  </div>
-  <div id="section42" class="container-fluid">
-  <center><h1>Valores</h1>
-  <p><h4>Presta serviços com resposabilidade</p>
-  <p>Ser transparente com as noticais</p>
-  <p>trazre beneficios para os cidadões </h4></p></center>
-  </div> -->
- 
-<div class="container">
-  <h2>Deixe seu comentario </h2>
-  <!--<p>Use the "media-left" class to left-align a media object. Text that should appear next to the image, is placed inside a container with class="media-body".</p>
-  <p>Tip: Use the "media-right" class to right-align the media object.</p><br>
   
-  <!-- Left-aligned media object -->
-  <div class="media">
-    <div class="media-left">
-      <img src="img/figura04.png" class="media-object" style="width:60px">
-    </div>
-    <div class="media-body">
-      <h4 class="media-heading">Maria dos Santos</h4>
-      <p>Gostaria de saber quando vai haver doacões de leite? </p>
-    </div>
+  <div class="container">
+    <h2>Dia-a-dia Associações</h2>
+    <!-- <p>Add class .active to let the user know which page he/she is on:</p> -->
+    <ul class="pagination">
+      <li><a href="#">1</a></li>
+      <li class="active"><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
+      <li><a href="#">4</a></li>
+      <li><a href="#">5</a></li>
+    </ul>
+
   </div>
-  <hr>
-  <div class="media">
-    <div class="media-left">
-      <img src="img/figura05.png" class="media-object" style="width:60px">
-    </div>
-    <div class="media-body">
-      <h4 class="media-heading">Fernanda Santos</h4>
-      <p>Gostei bastante do curso de manicure oferecido pela associação do Pilar</p>
-    </div>
-  </div>
-  <hr>
-  
-  <!-- Right-aligned media object -->
-  <div class="media">
-    <div class="media-body">
-      <h4 class="media-heading">José Antonio</h4>
-      <p> como faço para me cadidata para oferecer serviços comunitarios? </p>
-    </div>
-    <div class="media-right">
-      <img src="img/figura06.png" class="media-object" style="width:60px">
-    </div>
-  </div>
-</div>
-<br>>
-<br>
- <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-  <h2>Dia-a-dia Associações</h2>
- <!-- <p>Add class .active to let the user know which page he/she is on:</p> -->
-  <ul class="pagination">
-    <li><a href="#">1</a></li>
-    <li class="active"><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-  </ul>
-</div>
+</main>
+
+<?php include "rodape.php";
+?> 
 
 </body>
 </html>
