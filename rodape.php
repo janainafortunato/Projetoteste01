56d852bb1d2c32c47a28e50436400ae6eb8eeab9
suellennn <suellenbarbosa.c@hotmail.com>
2018-05-10 06:51:16 -0300

    ‘rodape’

--- rodape.php
+++ rodape.php
@@ -2,23 +2,27 @@
   <div class="footer-top p-y-2">
     <div class="container-fluid">
       <div class="row">
-        
         <div class="col-md-12">
           <div class="footer-title text-xs-center">
-          <h4 class="p-b-1">Dia-a-dia Associações</h4>
-        </div>
-  </div><!--col-md-2-->
+            <h4 class="p-b-1">Dia-a-dia Associações</h4>
+          </div>
+        </div><!--col-md-2-->
       </div>
     </div>
   </div>
-  <div class="footer-middle">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-3 col-sm-6">
+ 
+  <div class="col-md-3 col-sm-3">
         <!--Column1-->
-        
+        <div class="footer-pad">
+          <h4>SIGA-NOS</h4>
+          <i class="fab fa-facebook-square fa-2x"></i>
+          <i class="fab fa-twitter-square fa-2x"></i>
+          <i class="fab fa-instagram fa-2x"></i>
+          <a href="https://github.com/suellennn/Dia-a-dia-Associa-es" target="-blank"><i class="fab fa-github-alt fa-2x"></i></a>
+        </div>
       </div>
-      <div class="col-md-3 col-sm-6">
+
+      <div class="col-md-3 col-sm-3">
         <!--Column1-->
         <div class="footer-pad">
           <h4></h4>
@@ -29,7 +33,7 @@
           </ul>
         </div>
       </div>
-      <div class="col-md-3 col-sm-6">
+      <div class="col-md-3 col-sm-3">
         <!--Column1-->
         <div class="footer-pad">
           <h4></h4>
@@ -40,7 +44,7 @@
           </ul>
         </div>
       </div>
-      <div class="col-md-3 col-sm-6">
+      <div class="col-md-3 col-sm-3">
         <!--Column1-->
         <div class="footer-pad">
           <h4></h4>
@@ -51,7 +55,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div3
   </div>
   <div class="footer-bottom">
     <div class="container-fluid">
