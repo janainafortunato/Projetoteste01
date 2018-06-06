878e10e7caedb6001f2c19e46039a3e031f230cf
janiio <janiovinicius17@gmail.com>
2018-06-06 13:09:38 -0300

    desenvolvedores

--- cabecalho.php
+++ cabecalho.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" type="text/css" href="fontawesome/web-fonts-with-css/css/fontawesome-all.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
--- desenvolvedores.php
+++ desenvolvedores.php
@@ -1,8 +1,60 @@
 <?php
 include 'cabecalho.php';
-
 ?>
-<div style="margin-top:450px;">
+<main>
+	
+<div class="container">
+  <div>
+  	<h1 class="titulo">Desenvolvedores</h1>
+  	<br>
+  </div> 
+  <div class="card-deck">
+  	<div class="card" style="width:400px">
+    	<img class="card-img-top" src="img/figura06.jpg" alt="Card image" style="width:100%">
+    	<div class="card-body">
+      	<h4 class="card-title">Suellen Barbosa</h4>
+				<p class="card-text"><i class="fas fa-at"></i> suellen1718@gmail.com</p>
+      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)98971-5026</p>
+      	<a href="https://github.com/suellennn"><i class="fab fa-github"></i></a>
+    	</div>
+  	</div>
+  	<div class="card" style="width:400px">
+    	<img class="card-img-top" src="img/figura07.jpg" alt="Card image" style="width:100%">
+    	<div class="card-body">
+      	<h4 class="card-title">Jânio Vinicius</h4>
+      	<p class="card-text"><i class="fas fa-at"></i> janiovinicius17@gmail.com</p>
+      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)98676-9119</p>
+      	<a href="https://github.com/janiio"><i class="fab fa-github"></i></a>
+      	
+    	</div>
+  	</div>
+  	<div class="card" style="width:400px">
+    	<img class="card-img-top" src="img/figura08.jpg" alt="Card image" style="width:100%">
+    	<div class="card-body">
+      	<h4 class="card-title">Janaina Fortunato</h4>
+      	<p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
+      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)99799-3278</p>
+      	<a href="https://github.com/janainafortunato"><i class="fab fa-github"></i></a>
+
+    	</div>
+  	</div> 
+  	<div class="card" style="width:400px">
+    	<img class="card-img-top" src="img/figura10.jpg" alt="Card image" style="width:100%">
+    	<div class="card-body">
+      	<h4 class="card-title">Camila Rodrigues</h4>
+      	<p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
+      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)98461-6717</p>
+      	<a href="https://github.com/camilars"><i class="fab fa-github"></i></a>
+
+    	</div>
+  	</div>
+  </div>
+</div>
+
+
+</main>
+
+
 <?php
 include 'rodape.php';
 ?>
