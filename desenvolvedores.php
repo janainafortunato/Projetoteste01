6ad27b04de3325421d311cb1c9112ad588abe43b
suellennn <suellenbarbosa.c@hotmail.com>
2018-06-07 11:52:44 -0300

    atualização

--- desenvolvedores.php
+++ desenvolvedores.php
@@ -2,57 +2,68 @@
 include 'cabecalho.php';
 ?>
 <main>
-	
 <div class="container">
   <div>
-  	<h1 class="titulo">Desenvolvedores</h1>
-  	<br>
+    <h1 class="titulo">Desenvolvedores</h1>
+    <br>
   </div> 
-  <div class="card-deck">
-  	<div class="card" style="width:400px">
-    	<img class="card-img-top" src="img/figura06.jpg" alt="Card image" style="width:100%">
-    	<div class="card-body">
-      	<h4 class="card-title">Suellen Barbosa</h4>
-				<p class="card-text"><i class="fas fa-at"></i> suellen1718@gmail.com</p>
-      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)98971-5026</p>
-      	<a href="https://github.com/suellennn"><i class="fab fa-github"></i></a>
-    	</div>
-  	</div>
-  	<div class="card" style="width:400px">
-    	<img class="card-img-top" src="img/figura07.jpg" alt="Card image" style="width:100%">
-    	<div class="card-body">
-      	<h4 class="card-title">Jânio Vinicius</h4>
-      	<p class="card-text"><i class="fas fa-at"></i> janiovinicius17@gmail.com</p>
-      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)98676-9119</p>
-      	<a href="https://github.com/janiio"><i class="fab fa-github"></i></a>
-      	
-    	</div>
-  	</div>
-  	<div class="card" style="width:400px">
-    	<img class="card-img-top" src="img/figura08.jpg" alt="Card image" style="width:100%">
-    	<div class="card-body">
-      	<h4 class="card-title">Janaina Fortunato</h4>
-      	<p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
-      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)99799-3278</p>
-      	<a href="https://github.com/janainafortunato"><i class="fab fa-github"></i></a>
 
-    	</div>
-  	</div> 
-  	<div class="card" style="width:400px">
-    	<img class="card-img-top" src="img/figura10.jpg" alt="Card image" style="width:100%">
-    	<div class="card-body">
-      	<h4 class="card-title">Camila Rodrigues</h4>
-      	<p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
-      	<p class="card-text"><i class="fab fa-whatsapp"></i> (81)98461-6717</p>
-      	<a href="https://github.com/camilars"><i class="fab fa-github"></i></a>
-
-    	</div>
-  	</div>
+	
+<div class="row">
+  <div class="col-md-3">
+    <div class="thumbnail">
+      <img src="img/figura06.jpg" alt="Lights" style="width:50.3%">
+      <div class="caption">
+        <h4 class="card-title">Suellen Barbosa</h4>
+        <p class="card-text"><i class="fas fa-at"></i> suellen1718@gmail.com</p>
+        <p class="card-text"><i class="fab fa-whatsapp"></i> (81)98971-5026</p>
+        <a href="https://github.com/suellennn"><i class="fab fa-github"></i></a>
+      </div>
+ 
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="thumbnail">
+      <img src="img/figura07.jpg" alt="Nature" style="width:38%">
+        <div class="caption">
+        <h4 class="card-title">Jânio Vinicius</h4>
+        <p class="card-text"><i class="fas fa-at"></i> janiovinicius17@gmail.com</p>
+        <p class="card-text"><i class="fab fa-whatsapp"></i> (81)98676-9119</p>
+        <a href="https://github.com/janiio"><i class="fab fa-github"></i></a>
+        </div>
+      
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="thumbnail">
+      
+        <img src="img/figura08.jpg" alt="Fjords" style="width:38%">
+        <div class="caption">
+         <h4 class="card-title">Janaina Fortunato</h4>
+        <p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
+        <p class="card-text"><i class="fab fa-whatsapp"></i> (81)99799-3278</p>
+        <a href="https://github.com/janainafortunato"><i class="fab fa-github"></i></a>
+        </div>
+     
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="thumbnail">
+      
+        <img src="img/figura10.jpg" alt="Fjords" style="width:40.5%">
+        <div class="caption">
+         <h4 class="card-title">Camila Rodrigues</h4>
+        <p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
+        <p class="card-text"><i class="fab fa-whatsapp"></i> (81)98461-6717</p>
+        <a href="https://github.com/camilars"><i class="fab fa-github"></i></a>
+        </div>
+     
+    </div>
   </div>
 </div>
-
-
+</div>
 </main>
+
 
 
 <?php
