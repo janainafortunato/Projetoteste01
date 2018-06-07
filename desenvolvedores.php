757747c443380aff0b362ec5d1e8ff407827ab81
suellennn <suellenbarbosa.c@hotmail.com>
2018-06-07 13:06:30 -0300

    git

--- desenvolvedores.php
+++ desenvolvedores.php
@@ -12,24 +12,24 @@ include 'cabecalho.php';
 <div class="row">
   <div class="col-md-3">
     <div class="thumbnail">
-      <img src="img/figura06.jpg" alt="Lights" style="width:50.3%">
+      <img src="img/figura06.jpg" alt="Lights" style="width:65.3%">
       <div class="caption">
         <h4 class="card-title">Suellen Barbosa</h4>
-        <p class="card-text"><i class="fas fa-at"></i> suellen1718@gmail.com</p>
-        <p class="card-text"><i class="fab fa-whatsapp"></i> (81)98971-5026</p>
-        <a href="https://github.com/suellennn"><i class="fab fa-github"></i></a>
+        <p class="card-text"><i class="fas fa-at" ></i> suellen1718@gmail.com</p>
+        <p class="card-text"><i class="fab fa-whatsapp "></i> (81)98971-5026</p>
+        <a href="https://github.com/suellennn"><i class="fab fa-github fa-2x" style="color: black" ></i></a>
       </div>
  
     </div>
   </div>
   <div class="col-md-3">
     <div class="thumbnail">
-      <img src="img/figura07.jpg" alt="Nature" style="width:38%">
+      <img src="img/figura07.jpg" alt="Nature" style="width:49.3%">
         <div class="caption">
         <h4 class="card-title">Jânio Vinicius</h4>
-        <p class="card-text"><i class="fas fa-at"></i> janiovinicius17@gmail.com</p>
-        <p class="card-text"><i class="fab fa-whatsapp"></i> (81)98676-9119</p>
-        <a href="https://github.com/janiio"><i class="fab fa-github"></i></a>
+        <p class="card-text"><i class="fas fa-at" ></i> janiovinicius17@gmail.com</p>
+        <p class="card-text"><i class="fab fa-whatsapp "></i> (81)98676-9119</p>
+        <a href="https://github.com/janiio"><i class="fab fa-github fa-2x" style="color: black" ></i></a>
         </div>
       
     </div>
@@ -37,12 +37,12 @@ include 'cabecalho.php';
   <div class="col-md-3">
     <div class="thumbnail">
       
-        <img src="img/figura08.jpg" alt="Fjords" style="width:38%">
+        <img src="img/figura08.jpg" alt="Fjords" style="width:48%">
         <div class="caption">
          <h4 class="card-title">Janaina Fortunato</h4>
-        <p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
+        <p class="card-text"><i class="fas fa-at" ></i> camila.rs122@gmail.com</p>
         <p class="card-text"><i class="fab fa-whatsapp"></i> (81)99799-3278</p>
-        <a href="https://github.com/janainafortunato"><i class="fab fa-github"></i></a>
+        <a href="https://github.com/janainafortunato"><i class="fab fa-github fa-2x" style="color: black" ></i></a>
         </div>
      
     </div>
@@ -50,12 +50,12 @@ include 'cabecalho.php';
   <div class="col-md-3">
     <div class="thumbnail">
       
-        <img src="img/figura10.jpg" alt="Fjords" style="width:40.5%">
+        <img src="img/figura10.jpg" alt="Fjords" style="width:52.5%">
         <div class="caption">
          <h4 class="card-title">Camila Rodrigues</h4>
-        <p class="card-text"><i class="fas fa-at"></i> camila.rs122@gmail.com</p>
-        <p class="card-text"><i class="fab fa-whatsapp"></i> (81)98461-6717</p>
-        <a href="https://github.com/camilars"><i class="fab fa-github"></i></a>
+        <p class="card-text"><i class="fas fa-at" ></i> camila.rs122@gmail.com</p>
+        <p class="card-text"><i class="fab fa-whatsapp "></i> (81)98461-6717</p>
+        <a href="https://github.com/camilars"><i class="fab fa-github fa-2x" style="color: black" ></i></a>
         </div>
      
     </div>
