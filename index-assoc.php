8437197422f6c095c7e5f3432652982b6cc2fd9e
camilars <camila.rs122@gmail.com>
2018-06-11 12:42:54 -0300

    incluído js

--- index-assoc.php
+++ index-assoc.php
@@ -176,5 +176,26 @@ include 'bd/conexao.php';
 include 'rodape.php';
 ?>
 <div>
+	<script src="">
+		$(document).ready(function(){
+$("#mytable #checkall").click(function () {
+        if ($("#mytable #checkall").is(':checked')) {
+            $("#mytable input[type=checkbox]").each(function () {
+                $(this).prop("checked", true);
+            });
+
+        } else {
+            $("#mytable input[type=checkbox]").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
+    });
+    
+    $("[data-toggle=tooltip]").tooltip();
+});
+
+		
+	</script>
+
 </body>
 </html>
